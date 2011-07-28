ecbdac0bf7658eee8bedb2d4168d28f1a088b7dd
Etienne Garnier <garnier.etienne@gmail.com>
2011-07-28 12:07:08 +0200

    remove old files

--- lib/rubycas-client-sinatra.rb
+++
@@ -1,9 +0,0 @@
-require "rubycas-client-sinatra/version"
-
-module Rubycas
-  module Client
-    module Sinatra
-      # Your code goes here...
-    end
-  end
-end
--- lib/rubycas-client-sinatra/version.rb
+++
@@ -1,7 +0,0 @@
-module Rubycas
-  module Client
-    module Sinatra
-      VERSION = "0.0.1"
-    end
-  end
-end
